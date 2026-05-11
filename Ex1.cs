c78c4d7fb4335f36b5ee60a517d59d932544a154
saire <saire@SAIREDDY_2005>
2026-05-11 13:01:55 +0530

    NEW

---
+++ Ex1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Ex1
+    {
+        public void Yourname()
+        {
+           
+            Console.WriteLine("your fullname is  saikumar Ponagandla ");
+        }
+        public void Yourname2(string firstname, string lastname)
+        {
+
+          Console.WriteLine(" your fullname is "+ firstname+" "+lastname);
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Ex1 yn = new Ex1();
+            yn.Yourname();
+            yn.Yourname2("saikumar", "Ponagandla");
+        }
+    }
+
+}
+
---
+++ Return.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Return
+    {
+        public int sum()
+        {
+            int a, b, c;
+            a = 10;
+            b = 20;
+            c = a + b;
+            return c;
+          }
+        /*  with return type without parameters */
+
+
+        static void Main(string[] args)
+        {
+            Return re = new Return();
+            int result = re.sum();
+        }
+    }
+}
