2de22f2ab7321842d085112ca6f5a015b82e6be4
saire <saire@SAIREDDY_2005>
2026-05-11 11:59:44 +0530

    Add project files.

---
+++ NoreturnWithoutPara.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    internal class NoreturnWithoutPara
+
+//        /*     No Return Type and also  Without Parameter    */
+//    {
+//        class oops
+//        {
+//            public void greet()
+//            {
+//                Console.WriteLine("hello Dot net Developer");
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            oops ob = new oops();
+//            ob.greet();
+
+//        }
+//    }
+//}
