2ac357445890710c641259f89d0285b84410ffd4
saire <saire@SAIREDDY_2005>
2026-05-13 11:28:42 +0530

    new code

---
+++ Array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace OOPS
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[3];
+            string [] names = new string[3];
+            int[ ] age = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter the marks:");
+                marks [i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Name:");
+                names [i] = Console.ReadLine();
+                Console.WriteLine("Enter the age:");
+                age[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter the Student name search");
+             string search = Console.ReadLine();
+            for (int j = 0; j < 3; j++)
+            {
+                if (search == names[j])
+                {
+                    Console.WriteLine("Student name is:"+names[j]);
+                    Console.WriteLine("Student Marks is :"+marks[j]);
+                    Console.WriteLine("Student age is :" + age[j]);
+
+                }
+            }
+        } 
+    }
+}
--- StudentwithArray.cs
+++ StudentwithArray.cs
@@ -1,56 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    internal class StudentwithArray
-    {
-        class student
-        {
-            int id;
-            string name;
-            int age;
-            static string college = "abc college";
+//namespace OOPS
+//{
+//    internal class StudentwithArray
+//    {
+//        class student
+//        {
+//            int id;
+//            string name;
+//            int age;
+//            static string college = "abc college";
 
-            public void Getdetails()
-            {
-                Console.WriteLine("Enter the Student Id:");
-                id =Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter the Student Name:");
-                name = Console.ReadLine();
-                Console.WriteLine("Enter the Student age:");
-                age = Convert.ToInt32(Console.ReadLine());
+//            public void Getdetails()
+//            {
+//                Console.WriteLine("Enter the Student Id:");
+//                id =Convert.ToInt32(Console.ReadLine());
+//                Console.WriteLine("Enter the Student Name:");
+//                name = Console.ReadLine();
+//                Console.WriteLine("Enter the Student age:");
+//                age = Convert.ToInt32(Console.ReadLine());
 
 
-            }
-            public void result()
-            {
-                Console.WriteLine("Student Id is:" + id);
-                Console.WriteLine("Student Name is:"+name);
-                Console.WriteLine("Student Age:" + age);
-                Console.WriteLine("Student college name" + college);
-            }
-            public static void DisplayCollege()
-            {
-                Console.WriteLine("College name is" + college);
-            }
-            static void Main(string[] args)
-            {
-                student[] stu = new student[2];
-                for (int i = 0; i < stu.Length; i++)
-                {
-                    stu[i] = new student();
-                    stu[i].Getdetails();
-                }
-                for (int i = 0; i < stu.Length; i++)
-                {
-                    stu[i].result();
-                }
-                //student.DisplayCollege();
-            }
-        }
-    }
-}
+//            }
+//            public void result()
+//            {
+//                Console.WriteLine("Student Id is:" + id);
+//                Console.WriteLine("Student Name is:"+name);
+//                Console.WriteLine("Student Age:" + age);
+//                Console.WriteLine("Student college name" + college);
+//            }
+//            public static void DisplayCollege()
+//            {
+//                Console.WriteLine("College name is" + college);
+//            }
+//            static void Main(string[] args)
+//            {
+//                student[] stu = new student[2];
+//                for (int i = 0; i < stu.Length; i++)
+//                {
+//                    stu[i] = new student();
+//                    stu[i].Getdetails();
+//                }
+//                for (int i = 0; i < stu.Length; i++)
+//                {
+//                    stu[i].result();
+//                }
+//                //student.DisplayCollege();
+//            }
+//        }
+//    }
+//}
