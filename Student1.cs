c880161c259887b938a8206c5662e373b4d7a1f2
saire <saire@SAIREDDY_2005>
2026-05-13 10:58:37 +0530

    commit

--- Student1.cs
+++ Student1.cs
@@ -1,15 +1,16 @@
 ﻿//using System;
 //using System.Collections.Generic;
 //using System.Linq;
+//using System.Runtime.Remoting.Services;
 //using System.Text;
-//using System.Threading;
 //using System.Threading.Tasks;
 
 //namespace OOPS
 //{
-//    internal class Student
+//    internal class Student1
 //    {
-//        public void StudentDetails() {
+//        public void StudentDetails()
+//        {
 //            Console.WriteLine("Enter the Student id: ");
 //            int id = Convert.ToInt32(Console.ReadLine());
 //            Console.WriteLine("Enter Student Name is:");
@@ -19,31 +20,51 @@
 //            for (int i = 1; i <= 10; i++)
 //            {
 //                StudentDe(id, name, age);
+//                Marks();
 //                StudentDetails();
+               
 
 //                /* it an static way of allocation */
-//                Student.College();
+//                Student1.College();
+              
 //            }
-            
+
+
 
 
 //        }
-//        public void StudentDe(int id,string name,int age) 
+//        public void StudentDe(int id, string name, int age)
 //        {
-//            Console.WriteLine("Student id is : "+id +"   Student name is : "+name+"   student age is : "+age);
+//            Console.WriteLine("Student id is : " + id + "   Student name is : " + name + "   student age is : " + age);
 
 //        }
 //        public static void College()
 //        {
 //            Console.WriteLine("RN REDDY IT SCHOOL");
 //        }
+//        public void Marks()
+//        {
+           
+//            Console.WriteLine("Enter the Total Stubjects:");
+//            int a = Convert.ToInt32(Console.ReadLine());
+//             for( int i=1;i<= a;i++)
+//            {
+//                Console.WriteLine("Enter each subject marks:");
+//                int marks = Convert.ToInt32(Console.ReadLine());
+//                a++;
+               
+//                Console.WriteLine( "total marks are :"+marks);
+//            }
+
+//        }
 //        static void Main(string[] args)
 //        {
- 
+
 //            {
-//                Student student = new Student();
+//                Student1 student = new Student1();
 //                student.StudentDetails();
-   
+               
+
 //            }
 //        }
 
