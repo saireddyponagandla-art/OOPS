7a274ef683e0b08ffd180297a2a2d91230efffea
saire <saire@SAIREDDY_2005>
2026-05-15 10:32:01 +0530

    add

--- ParameterwithDefalut.cs
+++ ParameterwithDefalut.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    internal class ParameterwithDefalut
-    {
-        class Employee
-        {
-            int eid;                //fields     or instance variable
-            string ename;      //instance variable
-            double esal;
-            public Employee()
-            {
-                //Default constructor
-                eid = 10;
-                ename ="sai";
-                esal = 50000;
+//namespace OOPS
+//{
+//    internal class ParameterwithDefalut
+//    {
+//        class Employee
+//        {
+//            int eid;                //fields     or instance variable
+//            string ename;      //instance variable
+//            double esal;
+//            public Employee()
+//            {
+//                //Default constructor
+//                eid = 10;
+//                ename ="sai";
+//                esal = 50000;
 
-            }
-            public Employee(int Eid, string Ename, double Esal)
-            {
-                //parameterized constructor
-                eid = Eid;
-                ename = Ename;
-                esal = Esal;
-            }
-            public void EmployeeDetails()
-            {
-                //display the outputs
-                Console.WriteLine("Employee id is:" + eid);
-                Console.WriteLine("Employee name is:" + ename);
-                Console.WriteLine("Employee salary is:" + esal);
-            }
+//            }
+//            public Employee(int Eid, string Ename, double Esal)
+//            {
+//                //parameterized constructor
+//                eid = Eid;
+//                ename = Ename;
+//                esal = Esal;
+//            }
+//            public void EmployeeDetails()
+//            {
+//                //display the outputs
+//                Console.WriteLine("Employee id is:" + eid);
+//                Console.WriteLine("Employee name is:" + ename);
+//                Console.WriteLine("Employee salary is:" + esal);
+//            }
             
-        }
-        static void Main(string[] args)
-        {
-            Employee em = new Employee();
-            em.EmployeeDetails();
-            Employee emp = new Employee(101,"suresh",35000);
-            emp.EmployeeDetails();
+//        }
+//        static void Main(string[] args)
+//        {
+//            Employee em = new Employee();
+//            em.EmployeeDetails();
+//            Employee emp = new Employee(101,"suresh",35000);
+//            emp.EmployeeDetails();
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
