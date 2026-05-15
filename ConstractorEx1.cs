c0c21457741a1b7bbe0b825bc29549bf2c08b5ee
saire <saire@SAIREDDY_2005>
2026-05-15 10:16:46 +0530

    add

--- ConstractorEx1.cs
+++ ConstractorEx1.cs
@@ -1,51 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
+//namespace OOPS
+//{
 
-    class Employee
-    {
-        int empid;
-        string empname;
-        float salary;
-        static string Companyname;
-        float hikeSalary = 0;
-        public Employee()
-        {
-            empid = 101;
-            empname = "sai kumar";
-            salary = 50000;
-            Companyname = "abc it solution";
-        }
-        public void hike()
-        {
-            float hikeSalary = 0;
-            hikeSalary = salary * 0.10f + salary;
+//    class Employee
+//    {
+//        int empid;
+//        string empname;
+//        float salary;
+//        static string Companyname;
+//        float hikeSalary = 0;
+//        public Employee()
+//        {
+//            empid = 101;
+//            empname = "sai kumar";
+//            salary = 50000;
+//            Companyname = "abc it solution";
+//        }
+//        public void hike()
+//        {
+//            float hikeSalary = 0;
+//            hikeSalary = salary * 0.10f + salary;
 
-            Console.WriteLine("After hike salary is:" + hikeSalary);
-        }
-        public void EmployeeDetails()
-        {
-            Console.WriteLine("Employee id is:" + empid);
-            Console.WriteLine("Employee name is:" + empname);
-            Console.WriteLine("Employee salary is:" + salary);
-            Console.WriteLine("Company name is:" + Companyname);
-           
-        }
+//            Console.WriteLine("After hike salary is:" + hikeSalary);
+//        }
+//        public void EmployeeDetails()
+//        {
+//            Console.WriteLine("Employee id is:" + empid);
+//            Console.WriteLine("Employee name is:" + empname);
+//            Console.WriteLine("Employee salary is:" + salary);
+//            Console.WriteLine("Company name is:" + Companyname);
+//            Console.WriteLine("Hike salary is:" + hikeSalary);
 
-    }
-    internal class ConstractorEx1
-    {
-        static void Main(string[] args)
-        {
-            Employee obj = new Employee();
-            obj.EmployeeDetails();
-            obj.hike();
-        }
-    }
-}
+//        }
+
+//    }
+//    internal class ConstractorEx1
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee obj = new Employee();
+//            obj.EmployeeDetails();
+//            obj.hike();
+//        }
+//    }
+//}
---
+++ ParameterEx.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    class Student
+//    {
+//        int sid;
+//        string sname;
+//        int sfee;
+//        static string schoolname = "ABC school";
+//        public Student(int Sid, string Sname, int Sfee)
+//        {
+//           sid = Sid;
+//            sname = Sname;
+//            sfee = Sfee;
+//        }
+//        public void StudentDetails()
+//        {
+//            Console.WriteLine("Student Id:" + sid);
+//            Console.WriteLine("Student name is :" + sname);
+//            Console.WriteLine("Student fee is:" + sfee);
+//            Console.WriteLine("Student College name is:" + schoolname);
+//        }
+//    }
+//    internal class ParameterEx
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student obj = new Student(101, "abc", 23000);
+          
+//            obj.StudentDetails();
+
+//            Student obj1 = new Student(102, "aaaaaaaa",23943);
+
+//            obj1.StudentDetails();
+//            Student obj2 =new Student(103,"bbb",45000); 
+//            obj2.StudentDetails();
+//        }
+//    }
+//}
