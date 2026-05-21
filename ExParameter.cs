c089d245d5523f8b82fbf4fc9541687d27fecbac
saire <saire@SAIREDDY_2005>
2026-05-21 10:34:47 +0530

    add

---
+++ ExParameter.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    class Program
+//    {
+//        public int id;
+//        public  string name;
+//         static string College;
+//        public Program()
+//        {
+//            id = 0;
+//            name = "saikumar";
+//            College = "ABC College";
+//        }
+//        public Program(int sid,string sname,string scollege)
+//        {
+//            id = sid;
+//            name = sname;
+//           College = scollege;
+//        }
+//        public void StudentD()
+//        {
+//            Console.WriteLine("Student id is: " + id);
+//            Console.WriteLine("Student name is: " + name);
+//            Console.WriteLine("Student college is: " + College);
+//        }
+//        public void Marks()
+//        {
+//            int M1 = 80, M2 = 90, M3 = 85;
+//            int total = M1 + M2 + M3;
+//            int avg = (M1 + M2 + M3) / 3;
+//            Console.WriteLine("Total marks is: " + total);
+//            Console.WriteLine("Average marks is: " + avg);
+//        }
+
+//    }
+//    internal class ExParameter
+//    {
+//        static void Main(string[] args)
+//        {
+//            Program p1 = new Program(1, "sai", "ABC College");
+//            Program p2 = new Program(2, "Raju", "ABC College");
+//            p1.StudentD();
+     
+//            p2.StudentD();
+//            Program p = new Program();
+//            p.StudentD();
+//        }
+
+//    }
+//}
---
+++ ThisKeyword.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+   
+        class ThisKey
+        {
+            int a;
+            int b;
+         
+        public  ThisKey(int a, int b)
+            {
+            this.a = a;
+            this.b = b;
+
+            }
+            public  void Add()
+            {
+            int c = a + b;
+                Console.WriteLine("sum of a &b is:" + c);
+            }
+        }
+    internal class ThisKeyword
+    {
+        static void Main(string[] args)
+        {
+            ThisKey th = new ThisKey(10,20);
+            th.Add();
+
+        }
+    }
+}
