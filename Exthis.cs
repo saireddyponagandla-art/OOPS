1bb3161e6f13e567f2c5ff751ade65b04da047a7
saire <saire@SAIREDDY_2005>
2026-05-21 11:23:42 +0530

    Add

---
+++ Exthis.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    class thisEx
+//    {
+//        public int id;
+//        public string name;
+//        public float sal;
+//        static string Company;
+
+//        public thisEx(int id, string name, float sal)
+//        {
+//            this.id = id;
+//            this.name = name;
+//            this.sal = sal;
+//            Company = "ABC Company";
+
+
+//        }
+//        public void Static()
+//        {
+//            Console.WriteLine("This is a Static KeyWord");
+//            Console.WriteLine(Company);
+//        }
+//        public void Details()
+//        {
+//            Console.WriteLine("Id is " + id);
+//            Console.WriteLine("Name is " + name);
+//            Console.WriteLine("Salary is" + sal);
+//            Console.WriteLine(" Company name is static name " + Company);
+//        }
+//    }
+
+
+
+//    internal class Exthis
+//    {
+//        static void Main(string[] args)
+//        {
+//            thisEx ob = new thisEx(101, "PAVAN", 35699.99F);
+//            ob.Static();
+//            ob.Details();
+
+
+//            //Another object 
+//            thisEx obj = new thisEx(102, "suresh", 444444);
+//            obj.Details();
+//            obj.Static();
+
+//        }
+
+
+//    }
+//}
+
---
+++ StuThis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+
+    class Student
+    {
+        int stid;
+        string stname;
+        static string college;
+        public  Student(int stid, string stname)
+        {
+            this.stid = stid;
+                this.stname = stname;
+            college = "  XYZ college";
+
+        }
+        //public void Static()
+        //{
+        //    Console.WriteLine("College Name is"+college);
+        //}
+        public void StuDet()
+        {
+            Console.WriteLine("Student id is " + stid);
+            Console.WriteLine("Student name is :" + stname);
+            Console.WriteLine("Student College name is " + college);
+        }
+    }
+    internal class StuThis
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student(23, "sai");
+            st.StuDet();
+            Student st1 = new Student(1, "asd");
+            st1.StuDet();
+           
+        }
+
+    }
+}
